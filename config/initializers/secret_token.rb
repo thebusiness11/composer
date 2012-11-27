d1cd46fee725aa3361b283e792ae343b80fa2cb8
Lance Velasco <lance@mlvgroup.com>
2012-11-26 21:51:18 -0800

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Composer::Application.config.secret_token = 'dde2e2ac6e860667a7973733982d48d97c70af8fcb03c0d8d83018ee58be6d4be7e9e451d070fc4e6f78632ec5c1d5efa05db0db90239e3a8350ee2d5b31984b'
